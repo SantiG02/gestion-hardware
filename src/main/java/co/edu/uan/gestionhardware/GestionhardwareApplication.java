4e669e6c321d7cdabdb82e3ccee9db5f65a1e5c3
Santiago Galindo <sgalindo80@uan.edu.co>
2026-08-13 01:13:48 -0500

    Configuracion inicial

---
+++ src/main/java/co/edu/uan/gestionhardware/GestionhardwareApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uan.gestionhardware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionhardwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionhardwareApplication.class, args);
+	}
+
+}
